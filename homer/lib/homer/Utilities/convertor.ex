cd24cbeadee403695aaff3717f8a3058e2b53263
florian-vuillemot <florian.vuillemot@epitech.eu>
2017-10-18 23:14:22 +0200

    Add utilities convertor of atom/string input

---
+++ homer/lib/homer/Utilities/convertor.ex
@@ -0,0 +1,13 @@
+defmodule Homer.Utilities.Convertor do
+  def get_correct_key(attrs, atom, nil_value \\ nil) do
+    res = case Map.has_key?(attrs, atom) do
+      true -> Map.get(attrs, atom)
+      _ -> Map.get(attrs, to_string(atom))
+    end
+
+    case is_nil(res) do
+      true -> nil_value
+      _ -> res
+    end
+  end
+end
