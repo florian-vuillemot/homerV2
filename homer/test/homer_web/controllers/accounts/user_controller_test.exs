1e56676b10e89541de6579d9eeccd147b4719659
florian-vuillemot <florian.vuillemot@epitech.eu>
2017-10-19 19:15:11 +0200

    #45 add auth test for admin

--- homer/test/homer_web/controllers/accounts/user_controller_test.exs
+++ homer/test/homer_web/controllers/accounts/user_controller_test.exs
@@ -29,10 +29,10 @@ defmodule HomerWeb.Accounts.UserControllerTest do
 
   describe "create user" do
     test "renders user when data is valid", %{conn: conn} do
-      conn = HomerWeb.Accounts.LoginControllerTest.auth_user(conn)
       new_conn = post conn, accounts_user_path(conn, :create), user: @create_attrs
       assert %{"id" => id} = json_response(new_conn, 201)["user"]
 
+      conn = HomerWeb.Accounts.LoginControllerTest.auth_user(conn)
       new_conn = get conn, accounts_user_path(conn, :show, id)
       assert json_response(new_conn, 200)["user"] == %{
         "id" => id,
@@ -43,6 +43,17 @@ defmodule HomerWeb.Accounts.UserControllerTest do
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
+      conn = HomerWeb.Accounts.LoginControllerTest.auth_user(conn)
+      bad_attrs = @invalid_attrs ++ @miss_attrs
+      Enum.map(bad_attrs,
+        fn attrs ->
+          new_conn = post conn, accounts_user_path(conn, :create), user: attrs
+          assert json_response(new_conn, 422)["errors"] != %{}
+        end
+      )
+    end
+
+    test "no update admin", %{conn: conn} do
       conn = HomerWeb.Accounts.LoginControllerTest.auth_user(conn)
       bad_attrs = @invalid_attrs ++ @miss_attrs
       Enum.map(bad_attrs,
@@ -107,11 +118,6 @@ defmodule HomerWeb.Accounts.UserControllerTest do
       assert json_response(conn, 401)["message"] != %{}
     end
 
-    test "not allow create a user", %{conn: conn} do
-      conn = post conn, accounts_user_path(conn, :create), user: @create_attrs
-      assert json_response(conn, 401)["message"] != %{}
-    end
-
     test "not allow to update a user", %{conn: conn} do
       user = fixture(:user)
       conn = put conn, accounts_user_path(conn, :update, user), user: @update_attrs
@@ -122,6 +128,38 @@ defmodule HomerWeb.Accounts.UserControllerTest do
       user = fixture(:user)
       conn = delete conn, accounts_user_path(conn, :delete, user)
       assert json_response(conn, 401)["message"] != %{}
+    end
+
+    test "not allow to make admin", %{conn: conn} do
+      new_conn = post conn, accounts_user_path(conn, :create), user: @create_attrs
+      assert %{"id" => id} = json_response(new_conn, 201)["user"]
+
+      post conn, accounts_user_path(conn, :make_admin), id: id
+      assert false == Homer.Accounts.is_admin?(id)
+    end
+  end
+
+  describe "admin test" do
+    test "create admin", %{conn: conn} do
+      admin_conn = HomerWeb.Accounts.LoginControllerTest.auth_user(conn, true)
+      new_conn = post conn, accounts_user_path(conn, :create), user: @create_attrs
+      assert %{"id" => id} = json_response(new_conn, 201)["user"]
+
+      get admin_conn, accounts_user_path(admin_conn, :show, id)
+      assert false == Homer.Accounts.is_admin?(id)
+
+      post admin_conn, accounts_user_path(admin_conn, :make_admin), id: id
+      assert true == Homer.Accounts.is_admin?(id)
+    end
+
+    test "cant create", %{conn: conn} do
+      no_admin_conn = HomerWeb.Accounts.LoginControllerTest.auth_user(conn)
+
+      new_conn = post no_admin_conn, accounts_user_path(no_admin_conn, :create), user: @create_attrs
+      assert %{"id" => id} = json_response(new_conn, 201)["user"]
+
+      post no_admin_conn, accounts_user_path(no_admin_conn, :make_admin), id: id
+      assert false == Homer.Accounts.is_admin?(id)
     end
   end
 
